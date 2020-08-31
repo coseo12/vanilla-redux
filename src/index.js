0081146cf91211a6d90815aacbd88cdf3426e16c
coseo12 <momo_12@naver.com>
2020-08-31 10:48:47 +0900

    react init

--- src/index.js
+++ src/index.js
@@ -1,31 +1,5 @@
-import { createStore } from 'redux';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
-const add = document.querySelector('.add');
-const minus = document.querySelector('.minus');
-const number = document.querySelector('.number');
-const ADD = 'ADD';
-const MINUS = 'MINUS';
-
-const countModifier = (count = 0, { type }) => {
-  if (type === 'ADD') return count + 1;
-  else if (type === 'MINUS') return count - 1;
-  else return count;
-};
-
-const countStore = createStore(countModifier);
-
-const onChange = () => {
-  number.innerText = countStore.getState();
-};
-
-countStore.subscribe(onChange);
-
-const handleAdd = () => {
-  countStore.dispatch({ type: ADD });
-};
-const handleMinus = () => {
-  countStore.dispatch({ type: MINUS });
-};
-
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+ReactDOM.render(<App></App>, document.getElementById('root'));
